03ce24c9225cbdc717e35ba85e30f95b69b67644
Abakar-Issa-Ali <abakar-issa-ali@laplateforme.io>
2024-06-10 16:19:21 +0200

    Jour04 done

---
+++ Jour04/DeclarationVariable.java
@@ -0,0 +1,12 @@
+public class DeclarationVariable {
+
+    public static void main(String[] args) { 
+
+        int a; int b; int c ;  // Déclaration de trois variables de type int sur cette 
+        a = 0; b = 0; c = 0;  // Initialisation de trois variables de type int à la valeur 0
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
