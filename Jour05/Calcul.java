568f9c14da0ec1af09c54573e652a346d039cbee
Abakar-Issa-Ali <abakar-issa-ali@laplateforme.io>
2024-06-10 16:39:53 +0200

    Jour05 done

---
+++ Jour05/Calcul.java
@@ -0,0 +1,16 @@
+public class Calcul {
+    public static void main(String[] args) {
+       
+        int nombre = 7;   // Déclaration et initialisation de la variable nombre à 7
+        
+        System.out.println(nombre);
+        
+        nombre = nombre + 3;     // Ajout de 3 à la variable nombre
+        
+        System.out.println(nombre);  // Affichage de la nouvelle valeur de nombre
+        
+        nombre = nombre * 5;    // Multiplication de la valeur de nombre par 5
+        
+        System.out.println(nombre);     // Affichage de la valeur finale de nombre
+    }
+}
