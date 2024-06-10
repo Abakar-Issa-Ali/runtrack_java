9c5cddfe8190c9c81388a37f86d24c382b5ae2f6
Abakar-Issa-Ali <abakar-issa-ali@laplateforme.io>
2024-06-10 21:52:08 +0200

    Jour09 done

---
+++ Jour09/MyString.java
@@ -0,0 +1,12 @@
+public class MyString {
+    public static void main(String[] args) {
+        // Déclaration de quatre variables de type String de différentes façons
+        String str1 = "Première variable de type String";
+        String str2 = new String("Deuxième variable de type String");
+        String str3;
+        str3 = "Troisième variable de type String";
+        String str4;
+        str4 = new String("Quatrième variable de type String");
+
+    }
+}
