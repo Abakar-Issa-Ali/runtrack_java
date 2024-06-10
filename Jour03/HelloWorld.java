39e090231e64b17bd9ca33bed469f75255ee5394
Abakar-Issa-Ali <abakar-issa-ali@laplateforme.io>
2024-06-10 15:55:33 +0200

    Jour03 done

---
+++ Jour03/HelloWorld.java
@@ -0,0 +1,10 @@
+// Je déclare ici la classe publique HelloWorld
+public class HelloWorld {
+    /*
+      Cette méthode sera exécutée lorsque je lance le programme.
+      Elle affiche "Hello world" dans le terminal.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");  // Affiche le message "Hello world" dans le terminal
+    }
+}
